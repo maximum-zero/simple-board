4749850de8c17f0fe2ee35d88c54a5c6b48db336
maximum0 <maximum.zero95@gmail.com>
2025-05-06 03:37:03 +0900

    feat: spring boot 프로젝트 초기화 #5
    초기화에 추가된 기술
    - spring web
    - actuator
    - devtools
    - lombok

---
+++ src/main/java/org/maximum0/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,13 @@
+package org.maximum0.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBoardApplication.class, args);
+    }
+
+}
