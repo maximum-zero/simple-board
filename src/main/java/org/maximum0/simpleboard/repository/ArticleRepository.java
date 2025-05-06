a6342598392a0fa1101175af5f96c6e6cc69a972
maximum-zero <maximum.zero95@gmail.com>
2025-05-06 22:10:41 +0900

    feat: 게시판 API 만들기 - Data REST 적용 및 테스트 정의 (#4)
    - Spring Data REST + HAL Explorer 의존성 추가
    - Data REST 설정
    - Data REST 테스트 작성

--- src/main/java/org/maximum0/simpleboard/repository/ArticleRepository.java
+++ src/main/java/org/maximum0/simpleboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package org.maximum0.simpleboard.repository;
 
 import org.maximum0.simpleboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
--- src/main/java/org/maximum0/simpleboard/repository/CommentRepository.java
+++ src/main/java/org/maximum0/simpleboard/repository/CommentRepository.java
@@ -2,6 +2,8 @@ package org.maximum0.simpleboard.repository;
 
 import org.maximum0.simpleboard.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
